6747c587b0832fa42a9c6f7215a71c805842b03e
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-06-11 13:07:40 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,9 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на ul.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
-
-
-
-
 
 const galleryContainer = document.querySelector('.gallery');
 const galleryItemsMarkup = creatGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 galleryContainer.addEventListener('click', onGalleryItemsClick);
-// galleryContainer.addEventListener('click', onEscKeyPress);
 
 function creatGalleryItemsMarkup(galleryItems){
     return galleryItems.map(({preview, original, description}) => {
@@ -34,33 +22,29 @@ function creatGalleryItemsMarkup(galleryItems){
 
 function onGalleryItemsClick (evt){
     evt.preventDefault();
-console.log(evt.target)
-    if(evt.target.nodeName !== 'IMG'){
-        return;
+    if(!evt.target.classList.contains("gallery__image")){
+      return;
     }
-    const isItemImage = evt.target.classList.contains("gallery__image");
-    if(!isItemImage) return;
+     console.log(evt.target)
     const imgUrl = evt.target.dataset.source;
 
 const instance = basicLightbox.create(
-       `<img src = "${imgUrl}" width = "1200" height ="800" >`);
+       `<img src = "${imgUrl}" width = "1200" height ="800" >`, {
+        onShow: (instance) => {
+                  window.addEventListener('keydown', onEscKeyPress);
+                },
+                onClose: (instance) => {
+                  window.removeEventListener('keydown', onEscKeyPress);
+                },
+       }
+       );
 
        instance.show();
-       
+       function onEscKeyPress(evt) {
+            const ESC_KEY_CODE = 'Escape';
+            const isEscKey = evt.code === ESC_KEY_CODE;
+            if (!isEscKey) return;
+            instance.close();
+          }
 }
 
-// instance.close(onEscKeyPress);
-
-function onEscKeyPress(e) {
-    if (e.code !== 'Escape') return;
-    instance.close();
-    
-  }
-//   onEscKeyPress()
-
-//   onShow: (instance) => {
-//     window.addEventListener('keydown', onEscKeyPress);
-//   }
-//   onClose: (instance) => {
-//     window.removeEventListener('keydown', onEscKeyPress);
-//   }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = creatGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+galleryContainer.addEventListener('click', onGalleryItemsClick);
+
+
+
+function creatGalleryItemsMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+       return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    }).join('')
+};
+
+function onGalleryItemsClick(evt){
+    evt.preventDefault();
+    console.log(evt.target)
+        if(evt.target.nodeName !== 'IMG'){
+            return;
+        }
+        const isItemImage = evt.target.classList.contains("gallery__image");
+        if(!isItemImage) return;
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+
+ });
+
+
+
